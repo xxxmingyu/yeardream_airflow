694d13c58990994a2de49012b8b47cbc7bef4e58
mingyu <xxxmingyu@gmail.com>
2024-06-15 18:06:32 +0900

    homework final

--- dags/dags_bash_operator_decorator.py
+++ dags/dags_bash_operator_decorator.py
@@ -4,7 +4,8 @@ from airflow.decorators import task
 from airflow.operators.empty import EmptyOperator
 
 @dag(start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"), schedule="0 13 * * 5#2", catchup=False, tags=["homework"])
-
-@task
 def generate_dag():
-    EmptyOperator(task_id="decorator_t1")
+    EmptyOperator(task_id="task")
+@task
+def task():
+    EmptyOperator(task_id="task_task")
