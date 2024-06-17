6eff3d753559b50ff48b794b7d9602ed7aa8b1c7
mingyu <xxxmingyu@gmail.com>
2024-06-17 09:16:11 +0900

    homework edit

--- dags/dags_bash_operator_decorator.py
+++ dags/dags_bash_operator_decorator.py
@@ -3,9 +3,16 @@ import pendulum
 from airflow.decorators import task
 from airflow.operators.empty import EmptyOperator
 
-@dag(start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"), schedule="0 13 * * 5#2", catchup=False, tags=["homework"])
-def generate_dag():
-    EmptyOperator(task_id="task")
-@task
-def task():
-    EmptyOperator(task_id="task_task")
+@dag(dag_id='dags_bash_operator_decorator', start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"), schedule="0 13 * * 5#2", catchup=False, tags=["homework"])
+def dags_bash_operator_decorator():
+    bash_t1 = BashOperator(
+        task_id="decorator_t1",
+        bash_command="echo whoami"
+    )
+    bash_t2 = BashOperator(
+        task_id="decorator_t2",
+        bash_command="echo $HOSTNAME"
+    )
+    bash_t1 >> bash_t2
+
+dags_bash_operator_decorator()
