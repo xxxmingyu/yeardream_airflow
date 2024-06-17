1b8190aefa7b105a8373c5220eb8346c65e0f2e1
mingyu <xxxmingyu@gmail.com>
2024-06-17 09:26:37 +0900

    fruit edit

--- dags/dags_bash_operator_decorator.py
+++ dags/dags_bash_operator_decorator.py
@@ -3,7 +3,11 @@ import pendulum
 from airflow.decorators import task
 from airflow.operators.empty import EmptyOperator
 
-@dag(dag_id='dags_bash_operator_decorator', start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"), schedule="0 13 * * 5#2", catchup=False, tags=["homework"])
+@dag(dag_id='dags_bash_operator_decorator', 
+     start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"), 
+     schedule="0 13 * * 5#2", 
+     catchup=False, 
+     tags=["homework"])
 def dags_bash_operator_decorator():
     bash_t1 = BashOperator(
         task_id="decorator_t1",
