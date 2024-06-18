e8661608fc6db28d6f331597495a8c5dc2a88f2b
mingyu <xxxmingyu@gmail.com>
2024-06-19 01:02:25 +0900

    hw

--- plugins/sensors/seoul_api_date_column_sensor.py
+++ plugins/sensors/seoul_api_date_column_sensor.py
@@ -16,21 +16,40 @@ class SeoulApiDateSensorHw(BaseSensorOperator):
         '''
         super().__init__(**kwargs)
         self.http_conn_id = 'openapi.seoul.go.kr'
-        self.endpoint = '{{var.value.apikey_openapi_seoul_go_kr}}/json' + dataset_nm + '/1/5'  # 5건만 추출
+        self.endpoint = '{{var.value.apikey_openapi_seoul_go_kr}}/json' + dataset_nm  # 5건만 추출
         self.subcol_nm = subcol_nm
         self.column_nm = column_nm
 
     def poke(self, context):
         import requests
         import json
+        import datetime
         connection = BaseHook.get_connection(self.http_conn_id)
-        url = f'http://{connection.host}:{connection.port}/{self.endpoint}'
-        self.log.info(f'url: {url}')
-        response = requests.get(url)
-        contents = json.loads(response.text)
-        self.log.info(f'response: {contents}')
-        code = contents.get('CODE')
-        keys = contents.get('ListAirQualityByDistrictService').get('row')[0].get('MSRDATE')
-        self.log.info(f'keys: {keys}')
-        self.log.info(self.subcol_nm)
-        self.log.info(self.column_nm)
+        start_row = 1
+        end_row = 1000
+
+        while True:
+            if start_row == 1000001:
+                break
+            url = f'http://{connection.host}:{connection.port}/{self.endpoint}/{start_row}/{end_row}'
+            self.log.info(f'url: {url}')
+            response = requests.get(url)
+            contents = json.loads(response.text)
+            self.log.info(f'response: {contents}')
+            for i in self.subcol_nm:
+                contents = contents.get(i)
+            for i in range(start_row, end_row):
+                key = datetime.datetime.strptime(contents[i].get(self.column_nm), '%Y%m%d')
+                targetday = datetime(2024, 06, 18)
+
+                if key == targetday:
+                    self.log.info('지정된 날에 저장된 값이 있습니다')
+                    self.log.info(contents)
+                    return True
+
+                if key < targetday:
+                    self.log.info('지정된 날에는 저장된 값이 없습니다')
+                    return False
+                
+            start_row = end_row + 1
+            end_row = end_row + 1000
