108d221f61dc5808b12ac9f1ba98cb44fd909dc3
mingyu <xxxmingyu@gmail.com>
2024-06-19 00:11:25 +0900

    hw

--- plugins/sensors/seoul_api_date_column_sensor.py
+++ plugins/sensors/seoul_api_date_column_sensor.py
@@ -33,5 +33,5 @@ class SeoulApiDateSensorHw(BaseSensorOperator):
         contents = json.loads(response.text)
         self.log.info(f'response: {contents}')
         code = contents.get('CODE')
-        keys = contents.get('ListAirQualityByDistrictService').get('row').get('MSRDATE')
+        keys = contents.get('ListAirQualityByDistrictService').get('row')[0].get('MSRDATE')
         self.log.info(f'keys: {keys}')
